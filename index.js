12498d7a4a9aeec44c3168490a702b948d9e5402
yona l <04l.yona@gmail.com>
2018-01-13 06:01:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,10 @@ describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
-  })
+})
+
+describe("Height", () => {
+    it("is less than 40", () => {
+      expect(index.height).toBeLessThan(40)
+   })
+})
