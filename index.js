1436fd93f8555b70a510a3a45da3a9af952cc42a
yona l <04l.yona@gmail.com>
2018-01-13 06:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,10 @@ describe("Height", () => {
       expect(index.height).toBeLessThan(40)
    })
 })
+
+describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
+    })
+  })
